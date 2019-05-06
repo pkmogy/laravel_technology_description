8563414db87b44c23ff65e47d4a159aa97060e2c
s14113135 <s14113135@stu.edu.tw>
2019-05-06 12:45:03 +0800

    Request實作上半部

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -7,7 +7,18 @@ use Illuminate\Http\Request;
 class UserProfileController extends Controller
 {
     public function show(Request $request){
-        echo "show".$request->id;
+        echo "顯示路由參數:".$request->id."<br>";
+        //判斷路徑
+        if($request->is('admin/*')) {
+            echo "顯示路徑:" . $request->path() . "<br>";
+            //url方法將返回不帶查詢字符串的URL
+            echo $request->url(). "<br>";
+            //fullUrl方法包含查詢字符串
+            echo $request->fullUrl(). "<br>";
+        }
+        if ($request->isMethod('get')){
+            echo "這是GET";
+        }
     }
 
 }
